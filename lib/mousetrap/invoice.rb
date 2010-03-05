158a067ddee0f86c8be628aa58e2840064562f05
Cameron Cox <cameron@sproutbox.com>
2010-03-05 15:20:37 -0500

    added invoice model

---
+++ lib/mousetrap/invoice.rb
@@ -0,0 +1,28 @@
+module Mousetrap
+  class Invoice < Resource
+    attr_accessor \
+      :id,
+      :number,
+      :billing_date,
+      :created_at
+
+    protected
+
+    def self.plural_resource_name
+      'invoices'
+    end
+
+    def self.singular_resource_name
+      'invoice'
+    end
+
+    def self.attributes_from_api(attributes)
+      {
+        :id           => attributes['id'],
+        :number       => attributes['number'],
+        :billing_date => attributes['billingDatetime'],
+        :created_at   => attributes['createdDatetime']
+      }
+    end
+  end
+end
